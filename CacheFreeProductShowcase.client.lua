2ac13889934b08fd949db24b5983b9346688a6ca
OfficialFade <foxplaysyt64@gmail.com>
2026-08-30 11:38:04 -0500

    Repin cache-free showcase branding

--- CacheFreeProductShowcase.client.lua
+++ CacheFreeProductShowcase.client.lua
@@ -3,7 +3,7 @@
 
 local HttpService = game:GetService("HttpService")
 local cacheKey = HttpService:GenerateGUID(false):gsub("-", "")
-local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/b3cb802/ProductShowcase.client.lua?cache_free=" .. cacheKey
+local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/e24c970/ProductShowcase.client.lua?cache_free=" .. cacheKey
 local response = game:HttpGet(showcaseUrl)
 assert(type(response) == "string" and #response > 0, "The product showcase returned no source")
 local runShowcase = assert(loadstring(response), "The product showcase could not be compiled")
