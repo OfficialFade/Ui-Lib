43060cc06b1845d293dd4697fde207f0f4c651a7
OfficialFade <foxplaysyt64@gmail.com>
2026-08-29 23:02:46 -0500

    Pin fresh launcher to centered header showcase

--- FreshBuyerShowcase.client.lua
+++ FreshBuyerShowcase.client.lua
@@ -1,7 +1,7 @@
 -- Fresh buyer showcase loader
 -- Fetches the newest full showcase from GitHub with cache-busting enabled.
 
-local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/8755718/UltimateBuyerShowcase.client.lua"
+local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/6956a87/UltimateBuyerShowcase.client.lua"
 local cacheBust = tostring(os.time()) .. "_" .. tostring(math.random(100000, 999999))
 local source = game:HttpGet(showcaseUrl .. "?v=" .. cacheBust)
 local runShowcase = assert(loadstring(source), "Could not load the latest buyer showcase")
