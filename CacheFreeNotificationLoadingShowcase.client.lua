a09e9bf7f7bec8ccee0afa4074049484897a1e22
OfficialFade <foxplaysyt64@gmail.com>
2026-08-30 12:36:36 -0500

    Repin cache-free notification showcase

--- CacheFreeNotificationLoadingShowcase.client.lua
+++ CacheFreeNotificationLoadingShowcase.client.lua
@@ -2,7 +2,7 @@
 
 local HttpService = game:GetService("HttpService")
 local cacheKey = HttpService:GenerateGUID(false):gsub("-", "")
-local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/ce14998/NotificationLoadingShowcase.client.lua?cache_free=" .. cacheKey
+local showcaseUrl = "https://raw.githubusercontent.com/OfficialFade/Ui-Lib/aae8c5d/NotificationLoadingShowcase.client.lua?cache_free=" .. cacheKey
 local source = game:HttpGet(showcaseUrl)
 assert(type(source) == "string" and #source > 0, "The notification showcase returned no source")
 assert(loadstring(source), "The notification showcase could not be compiled")()
